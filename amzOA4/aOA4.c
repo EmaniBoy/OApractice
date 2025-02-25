27f9fe3982568ea96884bca368fd8062471534e0
EmaniBoy <karthikemani0321@gmail.com>
2025-02-25 14:45:27 -0600

    understanding problem

--- amzOA4/aOA4.c
+++ amzOA4/aOA4.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+// take the given list of digits
+// add adjacent elements together to generate the next row 
+// extract only the last digit (least significant digit) of the sum
+// repeat process until only two digits remain 
+// final two digits form encrypted number
+
 int main(){
     printf("hello world!");
     return 0;
