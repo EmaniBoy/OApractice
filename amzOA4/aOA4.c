ca9d7c0501ea746b5f00bd19373f9824001be18f
EmaniBoy <karthikemani0321@gmail.com>
2025-02-25 14:55:30 -0600

    OA4

--- amzOA4/aOA4.c
+++ amzOA4/aOA4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // take the given list of digits
 // add adjacent elements together to generate the next row 
@@ -6,7 +7,34 @@
 // repeat process until only two digits remain 
 // final two digits form encrypted number
 
+char* encryptNumber(int numbers[], int n){
+    while(n > 2){
+        for(int i = 0; i < n - 1; i++){
+            numbers[i] = (numbers[i] + numbers[i+1]) % 10;
+        }
+        n--;
+    }
+
+    char* result = (char*)malloc(3 * sizeof(char));
+    /*
+    alternative to sprintf:
+
+    result[0] = '0' + numbers[0];
+    result[1] = '0' + numbers[1];
+    result[2] = '\0';
+    */
+    sprintf(result, "%d%d", numbers[0], numbers[1]);
+
+    return result;
+}
+
+
 int main(){
-    printf("hello world!");
+    int numbers[] = {4, 5, 6, 7};
+    int n = sizeof(numbers) / sizeof(numbers[0]);
+
+    char* encrypted = encryptNumber(numbers, n);
+    printf("encrypted number: %s\n", encrypted);
+    free(encrypted);
     return 0;
 }
