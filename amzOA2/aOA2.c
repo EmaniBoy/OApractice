d811278ea2d0a2eac48b3844f8635289a5bc652c
EmaniBoy <karthikemani0321@gmail.com>
2025-02-25 14:27:02 -0600

    OA2

--- amzOA2/aOA2.c
+++ amzOA2/aOA2.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+int minStartingHealth(int power[], int n, int armor){
+    int total_damage = 0;
+    int max__damage = 0; 
+    int armor_reduction = 0; 
+
+    for(int i = 0; i < n; i++){
+        total_damage += power[i];
+
+        if(power[i] > max__damage){
+            max__damage = power[i];
+        }
+    }
+
+    if(armor < max__damage){
+        armor_reduction = armor;
+    } else {
+        armor_reduction = max__damage;
+    }
+    int min_health = total_damage - armor_reduction + 1;
+
+    return min_health;
+
+}
+
 int main(){
-    printf("hello world!");
+    int power[] = {1, 2, 6, 7};
+    int armor = 5;
+    int n = sizeof(power)/ sizeof(power[0]);
+
+    int result = minStartingHealth(power, n, armor);
+    printf("min starting health requiered is: %d\n", result);
+    
     return 0;
 }
