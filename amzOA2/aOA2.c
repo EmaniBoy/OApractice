15e3e726be11a6b164dd6ff7da08f0241c917318
EmaniBoy <karthikemani0321@gmail.com>
2025-02-25 14:30:13 -0600

    OA2 comments

--- amzOA2/aOA2.c
+++ amzOA2/aOA2.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+//funcrion for min initial health needed to win
 int minStartingHealth(int power[], int n, int armor){
     int total_damage = 0;
     int max__damage = 0; 
     int armor_reduction = 0; 
 
+    //calculate total damage and find max damage per round
     for(int i = 0; i < n; i++){
         total_damage += power[i];
 
@@ -13,11 +15,14 @@ int minStartingHealth(int power[], int n, int armor){
         }
     }
 
+    //calculate armor reduction
     if(armor < max__damage){
         armor_reduction = armor;
     } else {
         armor_reduction = max__damage;
     }
+
+    //compute min starting health requiered
     int min_health = total_damage - armor_reduction + 1;
 
     return min_health;
@@ -25,10 +30,12 @@ int minStartingHealth(int power[], int n, int armor){
 }
 
 int main(){
+    //input
     int power[] = {1, 2, 6, 7};
     int armor = 5;
     int n = sizeof(power)/ sizeof(power[0]);
 
+    //calculate & print result
     int result = minStartingHealth(power, n, armor);
     printf("min starting health requiered is: %d\n", result);
     
